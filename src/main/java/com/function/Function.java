b2d84c13aea8039e0362bd579b847e1bd4069781
Obaid-Rehman <obaidkhattak@gmail.com>
2022-06-22 14:38:54 +0500

    Checked mvn invoker

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -9,7 +9,16 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.Optional;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -33,6 +42,18 @@ public class Function {
         // Parse query parameter
         final String query = request.getQueryParameters().get("name");
         final String name = request.getBody().orElse(query);
+
+        InvocationRequest request2 = new DefaultInvocationRequest();
+        request2.setPomFile(new File("D:/DOCKER_TEST/pq-api-v2-java_eclipse_jre_lib/pom.xml"));
+        request2.setGoals(Collections.singletonList("clean"));
+
+        Invoker invoker = new DefaultInvoker();
+        invoker.setMavenHome(new File("C:/Program Files/apache-maven-3.8.6/bin/mvn"));
+        try {
+            InvocationResult result2 = invoker.execute(request2);
+        } catch (MavenInvocationException e) {
+            e.printStackTrace();
+        }
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
